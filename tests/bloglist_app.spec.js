698f057c8c93c5a2200a06227037224eb6841305
tmtemetm <>
2024-05-24 13:58:42 +0300

    Tehtävä 5.17

---
+++ tests/bloglist_app.spec.js
@@ -0,0 +1,25 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Bloglist app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Login to application')).toBeVisible()
+    await expect(page.getByText('Username')).toBeVisible()
+    await expect(page.getByText('Password')).toBeVisible()
+    await expect(page.locator('input[name="username"]')).toBeVisible()
+    await expect(page.locator('input[name="password"]')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+  })
+})
